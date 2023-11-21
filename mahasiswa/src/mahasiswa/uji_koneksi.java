c03c99dad5b0c4cb569408efdc3f5556066714a3
Tegar Satria Alam <117506176+tegarsatriaalam@users.noreply.github.com>
2023-11-21 10:22:18 +0700

    Add files via upload

---
+++ mahasiswa/src/mahasiswa/uji_koneksi.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mahasiswa;
+
+import java.sql.Connection;
+
+public class uji_koneksi {
+    public static void main(String[] args){
+        Connection c = koneksi.sambung_ke_db();
+    }
+    
+}
